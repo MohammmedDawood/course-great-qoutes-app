4490c114c2d468d49eb2cb08d1eab38e56a1a5d7
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-22 05:27:17 +0200

    Outputting Data on the "Details" Page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -3,7 +3,7 @@ import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Max", text: "Learning React is fun!" },
-  { id: "q1", author: "Maxmilian", text: "Learning React is great!" },
+  { id: "q2", author: "Maxmilian", text: "Learning React is great!" },
 ];
 
 function AllQuotes() {
