4b77a3487c85f7b7e3d7ec4e68c55f37c044fa67
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-22 05:21:31 +0200

    Adding Dummy Data & More Content

--- src/components/quotes/QuoteItem.js
+++ src/components/quotes/QuoteItem.js
@@ -1,4 +1,4 @@
-import classes from './QuoteItem.module.css';
+import classes from "./QuoteItem.module.css";
 
 const QuoteItem = (props) => {
   return (
@@ -9,9 +9,9 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <a className='btn'>
+      {/* <a className='btn'>
         View Fullscreen
-      </a>
+      </a> */}
     </li>
   );
 };
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -2,7 +2,10 @@ import React from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 function NewQuote() {
-  return <QuoteForm />;
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 }
 
 export default NewQuote;
