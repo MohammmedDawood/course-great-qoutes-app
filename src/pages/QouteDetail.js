08c43cb02794cbbe1055b9e49e83b4ac25715134
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-22 04:46:47 +0200

    use nested routes to render comments

--- src/pages/QouteDetail.js
+++ src/pages/QouteDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 function QouteDetail() {
   const params = useParams();
@@ -7,6 +8,9 @@ function QouteDetail() {
     <>
       <h1>QouteDetail</h1>
       <p>{params.qouteId}</p>
+      <Route path="/qoutes/:qouteId/comments">
+        <Comments />
+      </Route>
     </>
   );
 }
